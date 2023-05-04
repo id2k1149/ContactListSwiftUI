4836c17e15aba37f6d52d872a35ac05579fd5254
id2k1149 <id2k1149@gmail.com>
2023-05-04 10:48:39 -0400

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Max Franz Immelmann on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
